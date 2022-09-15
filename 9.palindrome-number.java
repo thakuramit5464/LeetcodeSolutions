6776d98a507711c25cc372baf58fced33b417b14
thakuramit5464 <thakuramit5464@gmail.com>
2022-09-15 21:33:08 +0530

    Easy problem 2 solved

---
+++ 9.palindrome-number.java
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=9 lang=java
+ *
+ * [9] Palindrome Number
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0 || (x!=0 && x%10==0))
+        return false;
+        int res = 0;
+           while(x>res){
+            res = res*10 + x%10;
+            x = x/10;
+           }
+        return (x==res || x==res/10);
+    }
+}
+// @lc code=end
+
