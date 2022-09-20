c825bcc99f8a833269759f96bf994d5253763056
thakuramit5464 <thakuramit5464@gmail.com>
2022-09-20 22:04:17 +0530

    Parenthesis solution

---
+++ Difficulty/easy/20.valid-parentheses.java
@@ -0,0 +1,28 @@
+package Difficulty.easy;
+import java.util.Stack;
+
+/*
+ * @lc app=leetcode id=20 lang=java
+ *
+ * [20] Valid Parentheses
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+	for (char c : s.toCharArray()) {
+		if (c == '(')
+			stack.push(')');
+		else if (c == '{')
+			stack.push('}');
+		else if (c == '[')
+			stack.push(']');
+		else if (stack.isEmpty() || stack.pop() != c)
+			return false;
+	}
+	return stack.isEmpty();
+    }
+}
+// @lc code=end
+
